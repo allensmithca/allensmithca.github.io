b0ec70dbf21c98832e94e5a865a2debed64648b3
Allen Smith <allensmithca@gmail.com>
2017-01-11 21:33:39 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+ $(document).ready(function () 
+        {
+            $('#fade1').fadeIn(3000);
+            $('#fade2').fadeIn(4500);
+            $('#fade3').fadeIn(5000);
+            $('#fade4').fadeIn(6000);
+            $('#fade5').fadeIn(7000);
+            $('#fade6').fadeIn(8000);
+            $('#fade7').fadeIn(9000);
+            $('#fade8').fadeIn(9500);
+            $('#fade9').fadeIn(9500);
+            
+           
+        });
