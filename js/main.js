12ed5c2c0bc3706d550948c32119b35a6a36b3b8
Allen Smith <allensmithca@gmail.com>
2017-01-13 11:35:57 -0500

    General update. Added portfolio

--- js/main.js
+++ js/main.js
@@ -9,6 +9,68 @@
             $('#fade7').fadeIn(9000);
             $('#fade8').fadeIn(9500);
             $('#fade9').fadeIn(9500);
+            $('#fade10').fadeIn(9500);
+            $('#fade11').fadeIn(9500);
+            $('#fade12').fadeIn(9500);
+            $('#fade13').fadeIn(9500);
+            $('#fade14').fadeIn(9500);
+            $('#fade15').fadeIn(9500);
+            $('#fade16').fadeIn(9500);
+            $('#fade17').fadeIn(9500);
+            $(".slidingDiv").hide();
+            $(".show_hide").show();
+            $('.show_hide').click(function()
+            {
+                $(".slidingDiv").slideToggle();
+            }); 
+
+            $(".slidingDiv1").hide();
+            $(".show_hide1").show();
+
+            $('.show_hide1').click(function()
+            {
+                $(".slidingDiv1").slideToggle();
+            });  
             
+             $(".slidingDiv2").hide();
+            $(".show_hide2").show();
+
+            $('.show_hide2').click(function()
+            {
+                $(".slidingDiv2").slideToggle();
+            }); 
+            
+             $(".slidingDiv3").hide();
+            $(".show_hide3").show();
+
+            $('.show_hide3').click(function()
+            {
+                $(".slidingDiv3").slideToggle();
+            }); 
+            
+            $(".slidingDiv4").hide();
+            $(".show_hide4").show();
+
+            $('.show_hide4').click(function()
+            {
+                $(".slidingDiv4").slideToggle();
+            });
+            
+            $(".slidingDiv5").hide();
+            $(".show_hide5").show();
+
+            $('.show_hide5').click(function()
+            {
+                $(".slidingDiv5").slideToggle();
+            }); 
+            
+            $(".slidingDiv6").hide();
+            $(".show_hide6").show();
+
+            $('.show_hide6').click(function()
+            {
+                $(".slidingDiv6").slideToggle();
+            });  
            
         });
+
